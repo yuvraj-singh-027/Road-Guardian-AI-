c5cf4f90f66891426120335c569fca19fbaf2fae
yuvraj-singh-027 <yuvrajmksingh20@gmail.com>
2026-09-01 19:33:40 +0530

    fix(map): switch from watermarked Carto tiles to clean ESRI World Dark Gray basemap

--- frontend/src/components/DigitalTwinMapView.jsx
+++ frontend/src/components/DigitalTwinMapView.jsx
@@ -290,9 +290,16 @@ export default function DigitalTwinMapView() {
 
     leafletMapObj.current = map;
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-      maxZoom: 20,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    // Watermark-free, key-free Dark Canvas Basemap
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+      maxZoom: 16,
+      attribution: '&copy; Esri, DeLorme, NAVTEQ, OpenStreetMap contributors'
+    }).addTo(map);
+
+    // Dark Map Reference Labels Overlay
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Reference/MapServer/tile/{z}/{y}/{x}', {
+      maxZoom: 16,
+      attribution: ''
     }).addTo(map);
 
     const polylines = {};
